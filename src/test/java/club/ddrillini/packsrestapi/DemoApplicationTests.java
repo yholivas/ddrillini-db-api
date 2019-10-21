dc596d68fe2d6f985d701121da2daf29ae99c179
Daniel Olivas <yholivas@gmail.com>
2019-10-21 15:45:53 -0400

    initial spring boot app

---
+++ src/test/java/club/ddrillini/packsrestapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package club.ddrillini.packsrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
