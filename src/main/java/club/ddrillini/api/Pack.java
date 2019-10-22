8efd4bb4061661162a6bc57d2e7aa8a0a0f841ff
Daniel Olivas <yholivas@gmail.com>
2019-10-22 15:10:27 -0400

    create and list api working

--- src/main/java/club/ddrillini/api/Pack.java
+++ src/main/java/club/ddrillini/api/Pack.java
@@ -1,26 +1,26 @@
 package club.ddrillini.api;
 
 public class Pack {
-	private final long id;
-	private final String name;
-	private final int count;
+    private final long id;
+    private final String name;
+    private final int count;
 
-	public Pack(long id, String name, int count) {
-		this.id = id;
-		this.name = name;
-		this.count = count;
-	}
+    public Pack(long id, String name, int count) {
+        this.id = id;
+        this.name = name;
+        this.count = count;
+    }
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public int getCount() {
-		return count;
-	}
+    public int getCount() {
+        return count;
+    }
 }
 
--- src/main/java/club/ddrillini/api/PackController.java
+++ src/main/java/club/ddrillini/api/PackController.java
@@ -1,6 +1,9 @@
 package club.ddrillini.api;
 
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,19 +11,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping(value = "/api")
 public class PackController {
-	private static Pack[] packs;
+    private static ArrayList<Pack> packs;
 
-	public PackController() {
-		packs = new Pack[4];
-		packs[0] = new Pack(1, "Mute Sims", 10);
-		packs[1] = new Pack(2, "Notice Me Benpai", 12);
-		packs[2] = new Pack(3, "Sudziosis", 16);
-		packs[3] = new Pack(4, "Rikame's Simfiles", 13);
-	}
+    public PackController() {
+        packs = new ArrayList<>(4);
+        packs.add(0, new Pack(1, "Mute Sims", 10));
+        packs.add(1, new Pack(2, "Notice Me Benpai", 12));
+        packs.add(2, new Pack(3, "Sudziosis", 16));
+        packs.add(3, new Pack(4, "Rikame's Simfiles", 13));
+    }
 
-	@RequestMapping(value = "/packs", method=RequestMethod.GET)
-	public Pack[] getPacks() {
-		return packs;
-	}
+    @RequestMapping(value = "/packs", method = RequestMethod.GET)
+    public Pack[] getPacks() {
+        return packs.toArray(new Pack[packs.size()]);
+    }
+
+    @RequestMapping(value = "/packs/{id}", method = RequestMethod.GET)
+    public Pack getPack(@PathVariable("id") long id) {
+        return packs.get((int)(--id));
+    }
+
+    @RequestMapping(value = "/packs", method = RequestMethod.POST)
+    public void createPack(@RequestBody Pack newPack) {
+        packs.add(newPack);
+    }
 }
